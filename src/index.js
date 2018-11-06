7a312c3e4a042c4902fe5bd804dac8b998c9478f
Igormot123 <igormot111@gmail.com>
2018-11-06 23:42:02 +0200

    Смотри index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -12,11 +12,47 @@ function playlist(state = []) {
     return state;
 }
 
+/**
+ * Вынеси все, что относится к стору в отдельную директорию.
+ * Такая структура должна быть:
+ *  - src
+ *      - components
+ *          - pages
+ *      - containers
+ *      - redux
+ *          - constants
+ *          - actions
+ *          - reducers
+ *
+ * Как-то так
+ *
+ *  createStore тоже в отдельный файл вынеси в редакс директорию
+ *
+ *  dphlxxsh1 - это что-то вроде сикрет кея, наверное. Его надо в отдельный файл вынести т добавить файл в гит игнор
+ *
+ *  <Image publicId={data.public_id } signUrl="true"> - signUrl="true" убери, оно ж без него работает, а браузер кидает ошибку в консоль
+ *
+ *  class App extends Component - нету стейта, сделай функциональный компонент
+ *
+ *  --------------
+ *  const initialState = {};
+ *  export function myReducer(state = initialState, action) - нету смысла объявляьть константу, иожно записать просто state = {}
+ *  --------------
+ *
+ *  + в одном случае ты пишешь "export const Request = () =>", в другом "export function myReducer(state = initialState, action)" - используй уже тогда только стрелочное описание
+ *
+ *  И попроси Аню какой-то нормальный дизайн нарисовать)
+ *
+ * **/
+
+
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
     myReducer,
     // playlist,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+/* eslint-enable */
 
 console.log('111111', store.getState());
 
