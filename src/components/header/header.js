d45b612894c92a015a4656f4afdb6d66c6d3d3a0
Stas Motorny <stasmotorny@gmail.com>
2019-12-01 14:02:39 +0200

    another change in cv

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import {NavLink} from "react-router-dom";
 import React, { Component } from "react";
 import logo from '../../icons/logo.png';
-import resume from '../../icons/resume2.pdf';
+import resume from '../../icons/resume.pdf';
 
 class Header extends Component {
     render() {
